625a91559f79da20a8cfb7c2e93b12c02e598af8
xnorikix <150209260+xnorikix@users.noreply.github.com>
2025-07-18 16:55:09 +0900

    Add files via upload

---
+++ run_uninstaller.ps1
@@ -0,0 +1,9 @@
+Start-Process "C:\Program Files (x86)\MOTEX\LanScopeAnClient\AnClientUninstaller.exe"
+Start-Sleep -Seconds 1
+$wshell = New-Object -ComObject WScript.Shell
+$wshell.AppActivate("LANSCOPE Client Uninstaller")
+Start-Sleep -Milliseconds 500
+$wshell.SendKeys("motex")
+$wshell.SendKeys("{TAB}")
+Start-Sleep -Milliseconds 200
+$wshell.SendKeys("{ENTER}")
