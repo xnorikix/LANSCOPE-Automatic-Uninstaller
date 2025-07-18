b69a68aee3ae01e6891c7314f31854b15e83b507
xnorikix <150209260+xnorikix@users.noreply.github.com>
2025-07-18 16:35:28 +0900

    Add files via upload

---
+++ LANSCOPE Uninstaller.bat
@@ -0,0 +1,1 @@
+powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File \"C:\Program Files (x86)\MOTEX\LanScope Client\Distribution\run_uninstaller.ps1\"' -Verb RunAs"
